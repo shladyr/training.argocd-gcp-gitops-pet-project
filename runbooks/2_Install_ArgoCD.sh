5a2d72b63e821250128300735386657afbc7b1af
serhii-hladyr <s.hladyr@platipusgaming.com>
2024-05-10 13:03:16 +0300

    Add files

--- runbooks/2_Install_ArgoCD.sh
+++ runbooks/2_Install_ArgoCD.sh
@@ -13,12 +13,18 @@ cj9yiciBmuDQqt0T
 # Create Port-Forward
 # Login to UI
 
+SERVER_URL                - https://kubernetes.default.svc
+Name-of-Cluster           - sirko-macbook-cluster
 Name-of-Project           - Web-App-Pet-Project
 Name-of-App               - web-app-demo-resume
 Name-of-NameSpace         - web-app-demo-resume-namespace
 Name-of-Role              - Web-App-Pet-Project-Admin-Role
 Name-of-Project-Role      - Web-App-Pet-Project-Admin-Role ?
 Name-of-Group             - Web-App-Pet-Project-Admin-Group
+Name-of-Repo-URL          - https://github.com/shladyr/training.argocd-gcp-gitops-pet-project
+
+$ kubectl create namespace web-app-demo-resume-namespace
+$ kubectl get ns web-app-demo-resume-namespace
 
 
 
